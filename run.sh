cf17652f160c3eaa02f814cce56511b9211b0754
bogdan <b.pyanzin@postgrespro.ru>
2026-05-10 20:07:03 +0700

    fix typos

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # shellcheck disable=SC2148
 rm publish/main.pdf
-#latexmk -lualatex -pdf -auxdir=build -outdir=publish  -lualatex main.tex
-lualatex -interaction=nonstopmode -aux-directory=build -output-directory=publish main.tex
+latexmk -lualatex -pdf -auxdir=build -outdir=publish  -lualatex main.tex
+#lualatex -interaction=nonstopmode -aux-directory=build -output-directory=publish main.tex
